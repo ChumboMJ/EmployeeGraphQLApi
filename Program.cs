6fee36b83ad5a5182a9e483443b8734ce0860342
tim-forste <tforste@gmail.com>
2024-11-09 15:05:10 -0800

    Register the Services, Query, and Schema

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
+using EmployeeGraphQLApi.Interfaces;
+using EmployeeGraphQLApi.Models;
+using EmployeeGraphQLApi.Query;
+using EmployeeGraphQLApi.Services;
+using GraphQL.Types;
+using GraphQL;
+using static EmployeeGraphQLApi.Query.EmployeeQuery;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,14 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IEmployeeService, EmployeeService>();
+builder.Services.AddSingleton<EmployeeDetailsType>();
+builder.Services.AddSingleton<EmployeeQuery>();
+builder.Services.AddSingleton<ISchema, EmployeeDetailSchema>();
+builder.Services.AddGraphQL(b => b
+    .AddAutoSchema<EmployeeQuery>()  // schema
+    .AddSystemTextJson());   // serializer
 
 var app = builder.Build();
 
