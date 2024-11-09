7d808345b9a98aaca5e651e07f54df3d28f43fdb
tim-forste <tforste@gmail.com>
2024-11-09 13:00:21 -0800

    Create sample data, hard-coded as list - for now.

--- Services/EmployeeService.cs
+++ Services/EmployeeService.cs
@@ -5,6 +5,27 @@ namespace EmployeeGraphQLApi.Services
 {
     public class EmployeeService : IEmployeeService
     {
+        // Hard-coded Employee and Department data
+        // TODO: Replace this with data from a database
+        private List<Employee> employees = new List<Employee>
+        {
+            new Employee(1, "John", 30, 1),
+            new Employee(2, "Ash", 25, 2),
+            new Employee(3, "Misty", 22, 2),
+            new Employee(4, "Tim", 28, 2),
+            new Employee(5, "Brock", 27, 3),
+            new Employee(6, "Jessie", 26, 3),
+            new Employee(7, "James", 25, 3),
+            new Employee(8, "Gary", 24, 3)
+        };
+
+        private List<Department> departments = new List<Department>
+        {
+            new Department(1, "HR"),
+            new Department(2, "IT"),
+            new Department(3, "Finance")
+        };
+
         public List<EmployeeDetails> GetEmployee(int empId)
         {
             throw new NotImplementedException();
