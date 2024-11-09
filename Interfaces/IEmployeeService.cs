46eae8c40af748da594c7abf0343641f2c04e540
tim-forste <tforste@gmail.com>
2024-11-09 12:51:06 -0800

    Create Employee Service Interface and Stub out Service

---
+++ Interfaces/IEmployeeService.cs
@@ -0,0 +1,11 @@
+﻿using EmployeeGraphQLApi.Models;
+
+namespace EmployeeGraphQLApi.Interfaces
+{
+    public interface IEmployeeService
+    {
+        public List<EmployeeDetails> GetEmployees();
+        public List<EmployeeDetails> GetEmployee(int empId);
+        public List<EmployeeDetails> GetEmployeeByDepartment(int deptId);
+    }
+}
---
+++ Services/EmployeeService.cs
@@ -0,0 +1,23 @@
+﻿using EmployeeGraphQLApi.Interfaces;
+using EmployeeGraphQLApi.Models;
+
+namespace EmployeeGraphQLApi.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        public List<EmployeeDetails> GetEmployee(int empId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EmployeeDetails> GetEmployeeByDepartment(int deptId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<EmployeeDetails> GetEmployees()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
